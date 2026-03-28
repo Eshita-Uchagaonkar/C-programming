86c0bc36807b28b1fb34e751f9bf70c131acaa01
Eshita-Uchagaonkar <eshitavu0512@gmail.com>
2026-03-28 17:30:41 +0000

    Changes

--- Assignments/Assignment18.c
+++ Assignments/Assignment18.c
@@ -1,1 +1,35 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+    char str1[100],str2[100],temp[100];
+    int length,i;
 
+    printf("Enter a string:");
+    scanf("%s",str1);
+
+    length=strlen(str1);
+    printf("\nLength of string:%d",length);
+    printf("Reversed string:");
+    for(i=length-1;i>=0;i--) {
+        printf("%c",str1[i]);}
+
+    
+    printf("\nEnter another string for comparison:");
+    scanf("%s",str2);
+    if(strcmp(str1,str2)==0)
+    printf("\nStrings are equal");
+    else
+    printf("\nNot equal\n");
+    
+    if(strcmp(str1,temp)==0)
+    printf("\nString is a palindrome");
+    else
+    printf("\nString is not a palindrom");
+
+    if(strstr(str1,str2)!=NULL)
+    printf("\nSecond string is a substring");
+    else
+    printf("\nSecond string is not a substring");
+
+return 0;
+}
---
+++ Assignments/Assignment19.c
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<string.h>
+
+struct EMPLOYEE {
+    char name[50];
+    char designation[50];
+    char gender[10];
+    char doj[20];
+    float salary;
+};
+
+void totalemployees(int n);
+void countgender(struct EMPLOYEE emp[],int n);
+void salaryabove10000(struct EMPLOYEE emp[],int n);
+void asstmanager(struct EMPLOYEE emp[],int n);
+
+int main() {
+    int n,i;
+
+    printf("Enter number of employees:");
+    scanf("%d",&n);
+
+    struct EMPLOYEE emp[n];
+
+    for(i=0;i<n;i++) {
+        printf("\nEnter details of employee %d\n",i=1);
+
+        printf("Name:");
+        scanf("%s",emp[i].name);
+        printf("\nDesignation:\n");
+        scanf("%s",emp[i].designation);
+        printf("\nGender:");
+        scanf("%s",emp[i].gender);
+        printf("\nDate of Joining:");
+        scanf("%s",emp[i].doj);
+        printf("\nSalary:");
+        scanf("%f", &emp[i].salary);
+    }
+    totalemployees(n);
+    countgender(emp,n);
+    salaryabove10000(emp,n);
+    asstmanager(emp,n);
+    return 0;
+}
+void totalemployees(int n) {
+    printf("\nTotal number of employees=%d\n",n);
+}
+void countgender(struct EMPLOYEE emp[],int n) {
+    int male=0,female=0,i;
+    for(i=0;i<n;i++) {
+        if(strcmp(emp[i].gender,"male")==0||strcmp(emp[i].gender,"male")==0)
+        male++;
+        else
+        female++;
+    }
+    printf("\nMale Employees=%d\n",male);
+    printf("\nFemale Employees=%d\n",female);
+}
+void salaryabove10000(struct EMPLOYEE emp[],int n) {
+    int i;
+    printf("\nEmployees with salary more than 10000:\n");
+    for(i=0;i<n;i++) {
+        if(emp[i].salary>10000) {
+            printf("%s\n",emp[i].name);
+        }
+    }
+}
+void asstmanager(struct EMPLOYEE emp[],int n) {
+    int i;
+    printf("\nEmployees with designation Asst Manager:\n");
+    for(i=0;i<n;i++) {
+        if(strcmp(emp[i].designation,"Asst Manager")==0) {
+            printf("%s\n",emp[i].name);
+        }
+    }
+}
